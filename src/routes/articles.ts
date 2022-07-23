763b3b7fd004ce46bed38976503ec4cc03bcbbb7
coderinblack08 <kevinlu.email@gmail.com>
2022-07-23 15:54:47 -0700

    feat: trending articles

--- src/routes/articles.ts
+++ src/routes/articles.ts
@@ -5,7 +5,7 @@ import type { ChildNode } from "domhandler";
 
 const articles = Router();
 
-articles.get("/trending", async (req, res) => {
+articles.get("/trending", async (_, res) => {
   const page = await fetch("https://medium.com");
   const $ = cheerio.load(await page.text());
   const response = [];
@@ -15,32 +15,49 @@ articles.get("/trending", async (req, res) => {
     function getAuthor(div: ChildNode) {
       const $$ = cheerio.load($.html(div));
 
-      console.log($$("div > a").attr("href")?.trim(), "\n");
-
       // test two different regex and get the one that matches
       const regex1 = /^\/@(.+)\?.*$/;
       const regex2 = /^https:\/\/medium.com\/(.+)\?.*$/;
       let username: string | undefined;
+      let displayName: string | undefined;
       let publication: string | undefined;
       const author = $$("div > a").attr("href")?.trim();
       if (author) {
         if (regex1.test(author)) {
           username = author.match(regex1)?.at(1);
+          displayName = $$("div > a").text();
         } else if (regex2.test(author)) {
           publication = author.match(regex2)?.at(1);
-          username = $$("div > div:nth-child(2) > a")
-            .attr("href")
-            ?.trim()
-            .match(regex1)
-            ?.at(1);
+          const el = $$("div > div:nth-child(2) > a");
+          displayName = el.children().first().text();
+          username = el.attr("href")?.trim().match(regex1)?.at(1);
         }
       }
 
-      return { username, publication };
+      return { username, displayName, publication };
     }
 
+    // function getMetadata(div: ChildNode) {
+    //   const $$ = cheerio.load($.html(div));
+    //   const publishedDate = $$("div > span:nth-child(1)").text();
+    //   const readingTime = $$("div > .pw-reading-time").text();
+    //   return { publishedDate, readingTime };
+    // }
+
+    const titleContent = cheerio.load($.html(title))("a > div > h2").text();
+
+    const url = cheerio
+      .load($.html(title))("a")
+      .attr("href")
+      ?.trim()
+      .split("?")[0];
+
     response.push({
+      url,
+      title: titleContent,
       author: getAuthor(author),
+      // medium broken when using a fetch request
+      // metadata: getMetadata(metadata),
     });
   }
   res.json(response);
